9d08738ced1804a073e52766b98b58cf4d442dea
Matheus Moreno <matheus.moreno@poli.ufrj.br>
2022-08-31 00:29:15 -0300

    refactor: change structure of pools for execution context

--- maestro/workflow/execution_context.py
+++ maestro/workflow/execution_context.py
@@ -1,26 +1,22 @@
 """Module with the execution context abstraction."""
 
 from dataclasses import dataclass
-from typing import List, Optional, Set
+import logging
+from typing import List, Optional
 
 from maestro.steps import Step
 
 
+LOGGER = logging.getLogger(__name__)
+
+
 @dataclass
 class StepContext:
-    """Data class for a step running in an workflow."""
+    """Data class for a step in a running workflow."""
 
     step: Step
     depends_on: List[str]
     failed_reason: Optional[str] = None
-
-    def __hash__(self) -> int:
-        """Return the step name as a hash."""
-        return hash(self.step.name)
-
-    def __eq__(self, __o: object) -> bool:
-        """Compare two steps."""
-        return self.step.name == getattr(__o, "step").name
 
 
 class ExecutionContext:
@@ -28,84 +24,66 @@ class ExecutionContext:
 
     def __init__(self) -> None:
         """Initialize execution context attributes."""
-        self._ready_steps: Set[StepContext] = set()
-        self._blocked_steps: Set[StepContext] = set()
-        self._successful_steps: Set[StepContext] = set()
-        self._failed_steps: Set[StepContext] = set()
-        self._current_step: StepContext
+        self.ready_steps: List[StepContext] = []
+        self.blocked_steps: List[StepContext] = []
+        self.successful_steps: List[StepContext] = []
+        self.failed_steps: List[StepContext] = []
+        self.current_step: StepContext
 
     @property
     def finished(self) -> bool:
         """Check if the workflow has finished its execution."""
-        return not bool(self._ready_steps)
+        return not bool(self.ready_steps)
 
     def register_step(self, step: Step) -> None:
         """Register a new step in the execution context."""
+        LOGGER.debug("Registering step %s.", step.name)
         step_ctx = StepContext(step=step, depends_on=step.depends_on)
-        queue = self._blocked_steps if step.depends_on else self._ready_steps
-        queue.add(step_ctx)
+        queue = self.blocked_steps if step.depends_on else self.ready_steps
+        queue.append(step_ctx)
 
     def get_next_step(self) -> Step:
         """Get next step ready for execution."""
-        self._current_step = self._ready_steps.pop()
-        return self._current_step.step
+        self.current_step = self.ready_steps.pop(0)
+        return self.current_step.step
 
     def set_current_step_as_successful(self) -> None:
         """Set current running step as successful."""
-        self._update_current_step(success=True)
+        self._update_current_step(failed=False)
         self._update_steps_dependent_on_successful_current_step()
 
     def set_current_step_as_failed(self, reason: str) -> None:
         """Set current running step as failed."""
-        self._update_current_step(success=False, reason=reason)
+        self._update_current_step(failed=True, reason=reason)
         self._update_steps_dependent_on_failed_current_step()
 
-    def format_final_results(self) -> str:
-        """Format text with final results for logging."""
-        text = "SUCCESSFUL STEPS:"
-        if self._successful_steps:
-            for step_ctx in self._successful_steps:
-                text += f"\n    |_ {step_ctx.step.name}"
-        else:
-            text += " []"
-
-        text += "\nFAILED STEPS:"
-        if self._failed_steps:
-            for step_ctx in self._failed_steps:
-                text += f"\n    |_ {step_ctx.step.name}"
-                text += f"\n       Reason: {step_ctx.failed_reason}"
-        else:
-            text += " []"
-
-        return text
-
-    def _update_current_step(self, success: bool, reason: str = None) -> None:
+    def _update_current_step(self, failed: bool, reason: str = None) -> None:
         """Update the running step as successful or failed."""
-        queue = self._successful_steps
-        if not success:
-            self._current_step.failed_reason = reason
-            queue = self._failed_steps
-        queue.add(self._current_step)
+        queue = self.successful_steps if not failed else self.failed_steps
+        self.current_step.failed_reason = reason
+        queue.append(self.current_step)
 
-    def _get_dependent_steps(self, step_ctx: StepContext) -> Set[StepContext]:
+    def _get_dependent_steps(self, step_ctx: StepContext) -> List[StepContext]:
         """Get set of dependent steps of a given step."""
-        return set(
-            blocked_step_ctx for blocked_step_ctx in self._blocked_steps
+        return [
+            blocked_step_ctx for blocked_step_ctx in self.blocked_steps
             if step_ctx.step.name in blocked_step_ctx.depends_on
-        )
+        ]
 
     def _update_steps_dependent_on_failed_current_step(self) -> None:
         """Set steps that depend on failed step as failed."""
-        reason = f"Depended on failed step {self._current_step.step.name}"
-        for step_ctx in self._get_dependent_steps(self._current_step):
+        reason = f"Depended on failed step {self.current_step.step.name}"
+        for step_ctx in self._get_dependent_steps(self.current_step):
+            LOGGER.debug("%s failed: %s", step_ctx.step.name, reason)
             step_ctx.failed_reason = reason
-            self._blocked_steps.remove(step_ctx)
-            self._failed_steps.add(step_ctx)
+            self.blocked_steps.remove(step_ctx)
+            self.failed_steps.append(step_ctx)
 
     def _update_steps_dependent_on_successful_current_step(self) -> None:
         """Update dependencies and queue newly indepedent steps."""
-        for step_ctx in self._get_dependent_steps(self._current_step):
-            step_ctx.depends_on.remove(self._current_step.step.name)
+        for step_ctx in self._get_dependent_steps(self.current_step):
+            step_ctx.depends_on.remove(self.current_step.step.name)
             if not step_ctx.depends_on:
-                self._blocked_steps.remove(step_ctx)
-                self._ready_steps.add(step_ctx)
+                LOGGER.debug("Step %s ready.", step_ctx.step.name)
+                self.blocked_steps.remove(step_ctx)
+                self.ready_steps.append(step_ctx)

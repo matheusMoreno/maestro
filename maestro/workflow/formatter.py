8536da129d1d5e5b3788e8d390e062925ac0e55a
Matheus Moreno <matheus.moreno@poli.ufrj.br>
2022-08-31 02:16:41 -0300

    refactor: remove redundant code in formatter

--- maestro/workflow/formatter.py
+++ maestro/workflow/formatter.py
@@ -1,6 +1,6 @@
 """Module with the execution log formatter."""
 
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 from maestro.workflow.execution_context import ExecutionContext
 
@@ -24,13 +24,13 @@ class ExecutionLogFormatter:
     def format(self) -> str:
         """Format output for a given execution."""
         return "\n\n".join([
-            self._format_title(),
+            self._format_header(),
             self._format_inputs(),
             self._format_steps(),
             self._format_outputs(),
         ])
 
-    def _format_title(self) -> str:
+    def _format_header(self) -> str:
         """Format title of results log."""
         title = f'     Workflow "{self._wf_name}"     '
         ornament = "=" * len(title)
@@ -38,23 +38,26 @@ class ExecutionLogFormatter:
 
     def _format_inputs(self) -> str:
         """Format inputs section of the result log."""
-        header = "Inputs:"
-        elements = [f"    - {k}: {v}" for k, v in self._wf_inputs.items()]
-        return "\n".join([header, *elements])
+        elements = [f"{k}: {v}" for k, v in self._wf_inputs.items()]
+        return self._format_as_list("Inputs", elements)
 
     def _format_steps(self) -> str:
         """Format steps section of the result log."""
-        header = "Steps:"
         step_list = self._context.successful_steps + self._context.failed_steps
         elements = [
-            f"    - {s.step.name}: SUCCESSFUL" if not s.failed_reason else
-            f"    - {s.step.name}: FAILED\n      |_ Reason: {s.failed_reason}"
+            f"{s.step.name}: SUCCESSFUL" if not s.failed_reason else
+            f"{s.step.name}: FAILED\n      |_ Reason: {s.failed_reason}"
             for s in step_list
         ]
-        return "\n".join([header, *elements])
+        return self._format_as_list("Steps", elements)
 
     def _format_outputs(self) -> str:
         """Format outputs section of the result log."""
-        header = "Outputs:"
-        elements = [f"    - {k}: {v}" for k, v in self._outputs.items()]
-        return "\n".join([header, *elements])
+        elements = [f"{k}: {v}" for k, v in self._outputs.items()]
+        return self._format_as_list("Outputs", elements)
+
+    @staticmethod
+    def _format_as_list(title: str, elements: List[str]) -> str:
+        """Format section as a list with a title."""
+        elements = [f"    - {e}" for e in elements]
+        return "\n".join([f"{title}:", *elements])
